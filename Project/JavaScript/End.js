1eb371ec7a512ca007787222d99e4f732a2eda5a
hsuanweifu <hsuanweifu@gmail.com>
2015-05-04 09:55:26 -0700

    update

--- Project/JavaScript/End.js
+++ Project/JavaScript/End.js
@@ -1,5 +1,5 @@
 // draw "Game Over" and the scores
-function drawText() {
+function drawEndPanelText(s_score, h_score) {
     //variables
     var c = document.getElementById("game");
     var ctx = c.getContext("2d");
@@ -21,13 +21,13 @@ function drawText() {
     gradient2.addColorStop("1", "#F45C43");
     ctx.fillStyle = gradient2;
     ctx.font = "bold " + width / 15 + "px Arial";
-    ctx.fillText("Your Score: " + "" + score, width / 4, width / 3);
+    ctx.fillText("Your Score: " + "" + s_score, width / 4, width / 3);
 
     // gradient3 and high score
     gradient3.addColorStop("0", "#4CB8C4");
     gradient3.addColorStop("1", "#3CD3AD");
     ctx.fillStyle = gradient3;
-    ctx.fillText("High Score: " + "" + Highscore, width / 4, width * 3 / 7);
+    ctx.fillText("High Score: " + "" + h_score, width / 4, width * 3 / 7);
 }
 
 // draw Restart button
@@ -68,49 +68,32 @@ function drawEndGameButton() {
     ctx.fillText(text, width * 3 / 11, width - width / 8);
 }
 
-// 
-function drawButtons() {
-    drawRestartButton();
-    drawEndGameButton();
-}
-/*
-function test() {
-    var canvas = document.getElementById('game');
-    var context = canvas.getContext('2d');
-    var width = canvas.width;
-    var x;
-    var y;
-    
-    canvas.addEventListener('click', function(evt) {
-      
-        x = evt.offsetX;
-        y = evt.offsetY;
-        
-        if (collision(width / 5, width - width / 2, width * 3 / 5, width / 5, x, y)) {
-            clearCanvas();
-            drawObservationPanel();
-        }
-        else {
-            alert("Not Passed");
-        }
-    }, false);
-}*/
-function test()
+//  Add end panel actions
+function addEndPanelActions()
 {
+    // variables
     var canvas = document.getElementById('game');
     var context = canvas.getContext('2d');
     var width = canvas.width;
+    
+    // restart button
     clickButton(width / 5, width - width / 2, width * 3 / 5, width / 5, function (){drawObservationPanel();});
+    // end game button
     clickButton(width / 5, width - width / 4, width * 3 / 5, width / 5, function (){drawStartPanel();});
 }
+/*
+*   Draw end panel
+*   @s_score the current score
+*   @h_score the highest score
+*/
 function drawEndPanel(s_score, h_score) {
+    // variables
     var canvas = document.getElementById('game');
     var context = canvas.getContext('2d');
     
-    score = s_score;
-    Highscore = h_score;
-    drawText();
-    drawButtons();
+    drawEndPanelText();
+    drawRestartButton();
+    drawEndGameButton(s_score, h_score);
 
-    test();
+    addEndPanelActions();
 }
