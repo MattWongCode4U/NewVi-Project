1fe89ea6ff6a1bba78742ce29c790c97431b679b
hsuanweifu <hsuanweifu@gmail.com>
2015-05-06 11:21:55 -0700

    Scaling canvas

---
+++ Project/JavaScript/Canvas.js
@@ -0,0 +1,43 @@
+function canvasResize() {
+    var 
+        // Obtain a reference to the canvas element
+        // using its id.
+        htmlCanvas = document.getElementById('game'),
+    
+        // Obtain a graphics context on the
+        // canvas element for drawing.
+        context = htmlCanvas.getContext('2d');
+
+    // Start listening to resize events and
+    // draw canvas.
+    initialize();
+ 
+    function initialize() {
+        // Register an event listener to
+        // call the resizeCanvas() function each time 
+        // the window is resized.
+        window.addEventListener('resize', resizeCanvas, false);
+        
+        // Draw canvas border for the first time.
+        resizeCanvas();
+    }
+				
+    // Display custom canvas.
+    // In this case it's a blue, 5 pixel border that 
+    // resizes along with the browser window.					
+    function redraw() {
+        //context.strokeStyle = 'blue';
+        //context.lineWidth = '5';
+        context.strokeRect(0, 0, window.innerWidth * 9 / 10, window.innerHeight * 9 / 10);
+    }
+		
+    // Runs each time the DOM window resize event fires.
+    // Resets the canvas dimensions to match window,
+    // then draws the new borders accordingly.
+    function resizeCanvas() {
+        htmlCanvas.width = window.innerWidth * 9 / 10;
+        htmlCanvas.height = window.innerHeight * 9 / 10;
+        redraw();
+    }
+		
+}
