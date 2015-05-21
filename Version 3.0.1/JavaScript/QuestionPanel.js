3b8b38557ea3a8d7859dfb5c79d66b72f179f5d2
hsuanweifu <hsuanweifu@gmail.com>
2015-05-21 09:37:41 -0700

    minor text updates

--- Version 3.0.1/JavaScript/QuestionPanel.js
+++ Version 3.0.1/JavaScript/QuestionPanel.js
@@ -17,8 +17,9 @@ function drawQuestionPanel(currentSlide, box, correct) {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     
     // draw question
-    drawText(0.5, 0.3, 0.1, 'What is the ', 'black');
-    drawText(0.5, 0.4, 0.1, 'Box ' + box.boxNumber, 'white');
-    drawText(0.5, 0.5, 0.1, 'of', 'black');
-    drawText(0.5, 0.6, 0.1, 'Slide ' + currentSlide, 'white');
+    drawText(0.5, 0.23, 0.1, 'Question ' + currentSlide, 'black');
+    drawText(0.5, 0.35, 0.1, 'What is the ', 'black');
+    drawText(0.5, 0.45, 0.1, 'Box ' + box.boxNumber, 'white');
+    drawText(0.5, 0.55, 0.1, 'of', 'black');
+    drawText(0.5, 0.65, 0.1, 'Slide ' + currentSlide, 'white');
 }
