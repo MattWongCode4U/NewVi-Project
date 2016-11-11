eef9f0b0ef6cf7858469e6fea40ab898077d6be6
michael7791 <a782347791@hotmail.com>
2016-11-11 14:17:02 -0800

    Deleting Excess files

--- Test/JavaScript/CommonFunctions.js
+++
@@ -1,8 +0,0 @@
-// clear the game canvas
-function clearCanvas() {
-    // variables
-    var canvas = document.getElementById("game");
-	var ctx = canvas.getContext("2d");
-    // clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
